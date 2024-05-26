fe7847fc05c5bf77e2cbfcc1e5259ca911f467e3
Giovanni Sakti <giosakti@gmail.com>
2024-05-26 17:27:27 +0700

    Setup Xresources

--- reload-ubuntu.sh
+++ reload-ubuntu.sh
@@ -5,6 +5,7 @@ export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -sfv "$DOTFILES_DIR/rc/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/rc/.profile" ~
+ln -sfv "$DOTFILES_DIR/rc/.Xresources" ~
 ln -sfv "$DOTFILES_DIR/rc/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/rc/.tmux.conf" ~
 
