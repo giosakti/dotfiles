d77681d720fa3f5d88bf6c2af3c18cdcb8c4e490
Giovanni Sakti <giosakti@gmail.com>
2024-05-26 17:27:06 +0700

    update i3 config and set up polybar

--- reload-ubuntu.sh
+++ reload-ubuntu.sh
@@ -14,9 +14,15 @@ ln -sfv "$DOTFILES_DIR/rc/.config/i3/config" ~/.config/i3
 mkdir -p ~/.config/i3blocks
 ln -sfv "$DOTFILES_DIR/rc/.config/i3blocks/config" ~/.config/i3blocks
 
+mkdir -p ~/.config/polybar
+ln -sfv "$DOTFILES_DIR/rc/.config/polybar/config.ini" ~/.config/polybar
+ln -sfv "$DOTFILES_DIR/rc/.config/polybar/launch.sh" ~/.config/polybar
+
 mkdir -p ~/.config/rofi
 ln -sfv "$DOTFILES_DIR/rc/.config/rofi/config" ~/.config/rofi
 
 ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
 ln -sfv "$DOTFILES_DIR/vim/.vim" ~
 mkdir -p ~/.vim/swapfiles
+
+sudo usermod -aG video ${USER}
