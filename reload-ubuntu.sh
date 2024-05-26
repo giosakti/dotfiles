1395d7f234309bfccd050a1129e8747db1381779
Giovanni Sakti <giosakti@gmail.com>
2024-05-26 21:33:57 +0700

    update link with absolute path

--- reload-ubuntu.sh
+++ reload-ubuntu.sh
@@ -3,23 +3,23 @@
 # Get current dir (so run this script from anywhere)
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -sfv "$DOTFILES_DIR/rc/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/rc/.profile" ~
-ln -sfv "$DOTFILES_DIR/rc/.Xresources" ~
-ln -sfv "$DOTFILES_DIR/rc/.zshrc" ~
-ln -sfv "$DOTFILES_DIR/rc/.tmux.conf" ~
+ln -sfv "$DOTFILES_DIR/rc/.bash_profile" ~/.bash_profile
+ln -sfv "$DOTFILES_DIR/rc/.profile" ~/.profile
+ln -sfv "$DOTFILES_DIR/rc/.tmux.conf" ~/.tmux.conf
+ln -sfv "$DOTFILES_DIR/rc/.Xresources" ~/.Xresources
+ln -sfv "$DOTFILES_DIR/rc/.zshrc" ~/.zshrc
 
 mkdir -p ~/.config/i3
-ln -sfv "$DOTFILES_DIR/rc/.config/i3/config" ~/.config/i3
+ln -sfv "$DOTFILES_DIR/rc/.config/i3/config" ~/.config/i3/config
 
 mkdir -p ~/.config/polybar
-ln -sfv "$DOTFILES_DIR/rc/.config/polybar/config.ini" ~/.config/polybar
-ln -sfv "$DOTFILES_DIR/rc/.config/polybar/launch.sh" ~/.config/polybar
+ln -sfv "$DOTFILES_DIR/rc/.config/polybar/config.ini" ~/.config/polybar/config
+ln -sfv "$DOTFILES_DIR/rc/.config/polybar/launch.sh" ~/.config/polybar/launch.sh
 
 mkdir -p ~/.config/rofi
-ln -sfv "$DOTFILES_DIR/rc/.config/rofi/config" ~/.config/rofi
+ln -sfv "$DOTFILES_DIR/rc/.config/rofi/config" ~/.config/rofi/config
 
-ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~/.vimrc
 ln -sfv "$DOTFILES_DIR/vim/.vim" ~
 mkdir -p ~/.vim/swapfiles
 
